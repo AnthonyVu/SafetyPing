81cd33790c9f4dc0054b7b4c544dae46dd939643
anthony vu <anthony_vu@live.ca>
2018-03-30 22:46:20 -0700

    Devices able to send name, lat, and long to server

--- app/src/main/java/com/example/antho/gps/MainActivity.java
+++ app/src/main/java/com/example/antho/gps/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.antho.gps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,67 +15,68 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TcpClient mTcpClient;
-    Button connect;
-    Button message;
-    Button quit;
-    EditText ip;
-    EditText port;
-    String convertedIp;
-    String loc;
-    int convertedPort;
+    private TcpClient mTcpClient;
+    private Button connect;
+    private Button disconnect;
+    private EditText ip;
+    private EditText port;
+    private EditText name;
+    private String convertedIp;
+    private String message;
+    private String convertedName;
+    private int convertedPort;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private boolean set1, set2, set3, connected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        set1 = false;
+        set2 = false;
+        set3 = false;
+        connected = false;
         connect = (Button) findViewById(R.id.connect);
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ConnectTask().execute("");
-                LocationManager locationManager = (LocationManager) getSystemService(v.getContext().LOCATION_SERVICE);
+                if(set1 && set2 && set3) {
+                    new ConnectTask().execute("");
+                    connected = true;
+                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    locationListener = new MyLocationListener();
 
-                LocationListener locationListener = new MyLocationListener();
-                if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-                //disable button once clicked or if no ip/port specified
-                //enable when close button is clicked
-                //connect.setEnabled(false);
-            }
-        });
-        message = (Button)findViewById(R.id.message);
-        message.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mTcpClient != null) {
-                    mTcpClient.sendMessage(loc);
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+
+                    //disable button once clicked or if no ip/port specified
+                    //enable when close button is clicked
+                } else {
+                    Toast.makeText(MainActivity.this, "You left something blank", Toast.LENGTH_SHORT).show();
+                    connected = false;
                 }
             }
         });
-        quit = (Button)findViewById(R.id.quit);
-        quit.setOnClickListener(new View.OnClickListener() {
+        disconnect = (Button)findViewById(R.id.disconnect);
+        disconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mTcpClient != null) {
+                if (connected) {
                     mTcpClient.sendMessage("quit");
                     try {
                         Thread.sleep(500);
@@ -85,6 +84,9 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                     mTcpClient.stopClient();
+                    connected = false;
+                } else {
+                    Toast.makeText(MainActivity.this, "You're not connected", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -97,7 +99,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                convertedIp = s.toString();
+                if(s.length() == 0) {
+                    set1 = false;
+                    return;
+                } else {
+                    convertedIp = s.toString();
+                    set1 = true;
+                }
             }
 
             @Override
@@ -115,15 +123,37 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length()==0)
-                {
+                if(s.length()==0) {
+                    set2 = false;
                     return;
-                }
-                else
-                {
+                } else {
                     convertedPort = Integer.parseInt(s.toString());
+                    set2 = true;
                 }
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        name = (EditText)findViewById(R.id.name);
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()==0) {
+                    set3 = false;
+                    return;
+                } else {
+                    convertedName = s.toString();
+                    set3 = true;
+                }
             }
 
             @Override
@@ -132,28 +162,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStop() {
+        if(connected) {
+            super.onStop();
+            mTcpClient.sendMessage("disconnect");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mTcpClient.stopClient();
+        }
+    }
+
     /*---------- Listener class to get coordinates ------------- */
     private class MyLocationListener implements LocationListener {
 
         @Override
-        public void onProviderDisabled(String provider) {}
-
-        @Override
-        public void onProviderEnabled(String provider) {}
-
-        @Override
-        public void onLocationChanged(Location location) {
-            String longitude = "Longitude: " + location.getLongitude();
-            String latitude = "Latitude: " + location.getLatitude();
-            Log.e("Lat", latitude);
-            Log.e("Long", longitude);
-            /*------- To get city name from coordinates -------- */
-
-            loc = longitude + "\n" + latitude + "\n";
+        public void onProviderDisabled(String provider) {
+            Log.d("Lat", "disable");
         }
 
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {}
+        public void onProviderEnabled(String provider) {
+            Log.d("Lat", "enable");
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("Lat", "Location");
+            String latitude = Double.toString(location.getLatitude());
+            String longitude = Double.toString(location.getLongitude());
+            Log.e("Lat", latitude);
+            Log.e("Long", longitude);
+            message = latitude + " " + longitude + " " + convertedName.replace(" ", "");
+            mTcpClient.sendMessage(message);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d("Lat", "status");
+        }
     }
 
     public class ConnectTask extends AsyncTask<String, String, TcpClient> {
